378dacdfe4a8bfc6025a422a130bb864987cd1de
Jan Bartoš <bartoj42@cvut.cz>
2024-12-15 15:32:56 +0100

    Adjusted ItemData

--- ItemData.lua
+++ ItemData.lua
@@ -1,12 +1,32 @@
 itemLinks = 
 {
-        "item:51255",   "",             "item:51256",
-        "item:50724",   "",             "item:50613",
-        "item:51257",   "",             "item:50694",
-        "item:53489",   "",             "item:50699",
-        "item:51259",   "",             "item:50664",
-        ""          ,   "",             "item:50398",
-        ""          ,   "",             "item:50365",
-        "item:54582",   "",             "item:54588",
-        "item:50734",   "item:50719",   "item:50684"
+    ["Za's current"] = 
+    {
+        "item:51255",   ""          ,   "item:51256",
+        "item:50724",   ""          ,   "item:50613",
+        "item:51257",   ""          ,   "item:50694",
+        "item:53489",   ""          ,   "item:50699",
+        "item:51259",   ""          ,   "item:50664",
+        ""          ,   ""          ,   "item:50398",
+        ""          ,   ""          ,   "item:50365",
+        "item:54582",   ""          ,   "item:54588",
+        "item:50734",   "item:50719",   "item:50684",
+    },
+    --[[
+        Example of entry, 
+        For showing no item in place use ""
+        Empty slots are padding, they can be used to show extra items as well
+    ["Another entry"] = 
+    {
+        "Head"      ,   "Empty slot",   "Hands"     ,
+        "Neck"      ,   "Empty slot",   "Waist"     ,
+        "Shoulder"  ,   "Empty slot",   "Legs"      ,
+        "Back"      ,   "Empty slot",   "Feet"      ,
+        "Chest"     ,   "Empty slot",   "Finger"    ,
+        "Shirt"     ,   "Empty slot",   "Finger"    ,
+        "Tabard"    ,   "Empty slot",   "Trinket"   ,
+        "Wrist"     ,   "Empty slot",   "Trinket"   ,
+        "Main Hand" ,   "Off-hand"  ,   "item:50684",
+    },
+    ]]
 }
