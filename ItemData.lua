d938c6991f8b9646f9df6e054e145aa3325d839d
Jan Bartoš <bartoj42@cvut.cz>
2024-12-15 14:58:41 +0100

    Formating

--- ItemData.lua
+++ ItemData.lua
@@ -1,12 +1,12 @@
-    itemLinks = {
-        "item:51255", "", "item:51256", -- Row 1
-        "item:50724", "", "item:50613", -- Row 2
-        "item:51257", "", "item:50694", -- Row 3
-        "item:53489", "", "item:50699",
-        "item:51259", "", "item:50664",
-        "", "", "item:50398",
-        "", "", "item:50365",
-        "item:54582", "", "item:54588",
-        "item:50734","item:50719","item:50684"
-        -- Add more item links for rows 4-9 as needed
-    }
+itemLinks = 
+{
+        "item:51255",   "",             "item:51256",
+        "item:50724",   "",             "item:50613",
+        "item:51257",   "",             "item:50694",
+        "item:53489",   "",             "item:50699",
+        "item:51259",   "",             "item:50664",
+        ""          ,   "",             "item:50398",
+        ""          ,   "",             "item:50365",
+        "item:54582",   "",             "item:54588",
+        "item:50734",   "item:50719",   "item:50684"
+}
